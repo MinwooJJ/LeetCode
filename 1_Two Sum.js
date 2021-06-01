646fac1ed4f86ee39349a4dc7150ae29b1494837
Minwoo <minwoojung01@gmail.com>
2021-05-31 21:58:06 -0700

    Add 1_Two Sum

---
+++ 1_Two Sum.js
@@ -0,0 +1,16 @@
+// T: O(n), S: O(n)
+const twoSum = (nums, target) => {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const another = target - nums[i];
+
+    if (another in map) {
+      return [map[another], i];
+    }
+
+    map[nums[i]] = i;
+  }
+
+  return null;
+};
