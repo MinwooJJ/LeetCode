53ec36d4ab25cda55a847f91785e2b16c9dc929b
Minwoo <minwoojung01@gmail.com>
2021-06-01 22:07:23 -0700

    Add 387

---
+++ 387_First Unique Character in a String.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// T: O(n), S: O(1)
+var firstUniqChar = function (s) {
+    const m = new Map();
+    for (let index = 0; index < s.length; index++) {
+        if (m.has(s[index])) {
+            m.set(s[index], -1);
+        }
+        else {
+            m.set(s[index], index);
+        }
+    }
+    let firstUniqIndex = -1;
+    for (const [key, index] of m) {
+        if (index != -1) {
+            firstUniqIndex = index;
+            break;
+        }
+    }
+    return firstUniqIndex;
+};
