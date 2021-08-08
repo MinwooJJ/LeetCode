14273e734802d5ca3bb3f1ba474c81513cf3e3fc
Minwoo <minwoojung01@gmail.com>
2021-08-08 14:08:09 -0700

    Add 253

---
+++ 253. Move Zeros.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let prev = 0;
+    let temp = 0;
+    
+    // T: O(n)
+    for(let i=0; i<nums.length; i++) {
+        if(nums[i] !== 0) {
+            temp = nums[prev];
+            nums[prev] = nums[i];
+            nums[i] = temp;
+            
+            prev++;            
+        }
+    }
+    
+    return nums;
+};
